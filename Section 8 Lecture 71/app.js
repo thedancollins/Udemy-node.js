588c312aea4c198594b9db092413689d1aa41cf1
thedancollins <thedancollins@hotmail.com>
2019-02-21 09:35:01 -0600

    Section 8 Lecture 71 - nodemon

---
+++ Section 8 Lecture 71/app.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    
+
+    var result = '404 Error';
+    
+    if (req.url === '/') {
+        fs.createReadStream(__dirname + '/index.htm').pipe(res);
+    }
+
+    else if (req.url === '/api') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json' 
+        });
+        var obj = {
+            firstname: 'Johnny',
+            lastname: 'Doe'
+        }
+        res.end(JSON.stringify(obj));
+    }
+    else {
+        res.writeHead(404);
+        res.end();
+    }
+
+}).listen(1337, '127.0.0.1');
