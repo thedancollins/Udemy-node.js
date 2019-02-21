8ee6d9fdf381be4c76bf37a2e3ad0f3949c298bb
thedancollins <thedancollins@hotmail.com>
2019-02-21 14:33:57 -0600

    Section 9 Lecture 78 - ESTful APIs and JSON

---
+++ Section 9 Lecture 78/app.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 3000;
+
+var url_encoded = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.use('/', function(req, res, next) {
+    console.log('Request URL: ' + req.url);
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/api', function(req, res) {
+    res.json({ firstname: 'John', lastname: 'Doe'});
+});
+
+app.get('/person/:id', function (req, res) {
+    res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
+});
+
+app.post('/person/', url_encoded, function (req, res) {
+    res.send('Thank you');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+});
+
+app.post('/personjson', jsonParser, function(req, res) {
+    res.send('Thank you for the JSON data');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+});
+
+
+app.post('/api/person', jsonParser, function(req, res) {
+    //save to the database
+});
+
+app.delete('/api/person/:id', function (req, res) {
+    //delete from the database
+});
+
+app.listen(port);
