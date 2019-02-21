9e049b0bf2c6c6014b5dc0dd6c8bcd1c1a404c96
thedancollins <thedancollins@hotmail.com>
2019-02-21 15:01:35 -0600

    Section 9 Lecture 79 - structuring our application

---
+++ Section 9 Lecture 79/CommandLine.sh
@@ -0,0 +1,4 @@
+ npm install express-generator -g
+ express myapp
+ npm install
+ 
---
+++ Section 9 Lecture 79/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var apiController = require('./controllers/apicontroller.js');
+var htmlController = require('./controllers/htmlcontroller.js');
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.use('/', function(req, res, next) {
+    console.log('Request URL: ' + req.url);
+    next();
+});
+
+htmlController(app);
+
+apiController(app);
+
+app.listen(port);
